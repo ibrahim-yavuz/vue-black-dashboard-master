79bcfc8a826c200aa8cef83b957e314d5d86a774
RabaxVIII <82673185+RabaxVIII@users.noreply.github.com>
2021-06-03 18:17:56 +0300

    commit

--- src/pages/Icons.vue
+++ src/pages/Icons.vue
@@ -2,86 +2,110 @@
   <div class="card">
     <div class="card-header">
       <h5 class="title">Sipariş Ver</h5>
-      <p class="category">Burada seçtiğiniz türdeki dondurmanın siparişini verebilirsiniz.Tek yapmanız gereken sipariş vermek istediğiniz dondurmanın altına adeti girdikten sonra "Satın Al" butonuna tıklamanız!</p>
-      <p class="category">Bilgilendirme: Firmamızın sipariş miktarı minumum 900, maximum 90.000'dir.</p>
+      <p class="category">
+        Burada seçtiğiniz türdeki dondurmanın siparişini verebilirsiniz.Tek
+        yapmanız gereken sipariş vermek istediğiniz dondurmanın altına adeti
+        girdikten sonra "Satın Al" butonuna tıklamanız!
+      </p>
+      <p class="category">
+        Bilgilendirme: Firmamızın sipariş miktarı minumum 900, maximum
+        90.000'dir.
+      </p>
     </div>
     <div class="card-body all-icons">
       <div class="row">
-        
-        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+        <div
+          class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6"
+        >
           <div class="font-icon-detail">
             <img :src="cikolatali" />
             <p>Çikolatalı Dondurma</p>
-            <input type="number" min="900" step="900" v-model="cikolatali_adet" placeholder="Alınacak Adet"/>
+            <input
+              type="number"
+              min="900"
+              step="900"
+              v-model="cikolatali_adet"
+              placeholder="Alınacak Adet"
+            />
             <button type="submit" @click="cikolataliAl">Satın Al</button>
           </div>
         </div>
 
-        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+        <div
+          class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6"
+        >
           <div class="font-icon-detail">
             <img :src="cilekli" />
             <p>Çilekli Dondurma</p>
-            <input type="number" min="0" v-model="cilekli_adet" placeholder="Alınacak Adet"/>
-            <button type="submit" >Satın Al</button>
+            <input
+              type="number"
+              min="0"
+              v-model="cilekli_adet"
+              placeholder="Alınacak Adet"
+            />
+            <button type="submit">Satın Al</button>
           </div>
         </div>
 
-        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+        <div
+          class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6"
+        >
           <div class="font-icon-detail">
             <img :src="muzlu" />
             <p>Muzlu Dondurma</p>
-            <input type="number" min="0" v-model="muzlu_adet" placeholder="Alınacak Adet"/>
-            <button type="submit" >Satın Al</button>
+            <input
+              type="number"
+              min="0"
+              v-model="muzlu_adet"
+              placeholder="Alınacak Adet"
+            />
+            <button type="submit">Satın Al</button>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 <script>
-import cikolatali from "./icons/cikolatali_dondurma.png"
-import cilekli from "./icons/cilekli_dondurma.png"
-import muzlu from "./icons/muzlu_dondurma.png"
+import cikolatali from "./icons/cikolatali_dondurma.png";
+import cilekli from "./icons/cilekli_dondurma.png";
+import muzlu from "./icons/muzlu_dondurma.png";
 export default {
-  data: function () {
-    return{
+  data: function() {
+    return {
       cikolatali: cikolatali,
       cilekli: cilekli,
       muzlu: muzlu,
       cikolatali_adet: 0,
       cilekli_adet: 0,
-      muzlu_adet: 0,
-    }    
+      muzlu_adet: 0
+    };
   },
   methods: {
-    cikolataliAl(){
-      if(this.cikolatali_adet >= 900 && this.cikolatali_adet < 90000){
-        this.$alert("Siparişiniz Alınmıştır.")
-      }else if(this.cikolatali_adet < 900){
-        this.$alert("Sipariş Alınamadı. Minumum Sipariş Adetimiz 900'dür.")
-      }else if(this.cikolatali_adet > 90000){
-        this.$alert("Sipariş Alınamadı. Maximum Sipariş Adetimiz 90000'dür.")
-      }else{
-        this.$alert("Sipariş Alınamadı. Lütfen Geçerli Bir Adet Giriniz.")
+    cikolataliAl() {
+      if (this.cikolatali_adet >= 900 && this.cikolatali_adet < 90000) {
+        this.$alert("Siparişiniz Alınmıştır.");
+      } else if (this.cikolatali_adet < 900) {
+        this.$alert("Sipariş Alınamadı. Minumum Sipariş Adetimiz 900'dür.");
+      } else if (this.cikolatali_adet > 90000) {
+        this.$alert("Sipariş Alınamadı. Maximum Sipariş Adetimiz 90000'dür.");
+      } else {
+        this.$alert("Sipariş Alınamadı. Lütfen Geçerli Bir Adet Giriniz.");
       }
     },
-    cilekliAl(){
-      if(this.cilekli_adet > 0){
-
+    cilekliAl() {
+      if (this.cilekli_adet > 0) {
       }
     },
-    muzluAl(){
-      if(this.muzlu_adet > 0){
-
+    muzluAl() {
+      if (this.muzlu_adet > 0) {
       }
     }
   }
 };
 </script>
-<style>
-
-input{
+<style scoped>
+input {
   background-color: transparent;
   color: white;
   border: none;
@@ -91,7 +115,7 @@ input{
   text-align: center;
 }
 
-button{
+button {
   outline: none;
   margin-top: 10px;
   border: none;
@@ -104,7 +128,7 @@ button{
   cursor: pointer;
 }
 
-button:focus{
+button:focus {
   outline: none;
 }
 
@@ -112,8 +136,7 @@ button:hover {
   background-color: #17172c;
 }
 
-button:active{
+button:active {
   background-color: #0a0a2b;
 }
-
 </style>
