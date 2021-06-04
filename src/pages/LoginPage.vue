9643c188b87753a16aa9a517611207bdbe455370
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2021-06-04 04:45:38 +0300

    commit

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,22 +1,62 @@
 <template>
   <div id="kapsayici">
-    <input v-model="kullanici_adi" placeholder="Kullanıcı Adı" />
-    <input v-model="sifre" placeholder="Şifre" type="password" />
-    <button @click="tiklama">Giriş Yap</button>
+    <div class="input-div">
+      <v-text-field
+        class="inputs"
+        filled
+        prepend-inner-icon="mdi-account"
+        dark
+        placeholder="Kullanıcı Adı"
+        v-model="kullanici_adi"
+      ></v-text-field>
+    </div>
+    <div class="input-div">
+      <v-text-field
+        class="inputs"
+        filled
+        prepend-inner-icon="mdi-lock"
+        dark
+        placeholder="Şifre"
+        v-model="sifre"
+        type="password"
+      ></v-text-field>
+    </div>
+    <div>
+      <v-btn @click="tiklama" class="ma-2" tile color="indigo" dark>
+        <v-icon left>mdi-login</v-icon>
+        Giriş Yap
+      </v-btn>
+    </div>
   </div>
 </template>
 <script>
+import vuetify from "@/plugins/vuetify";
 export default {
   data() {
     return {
       kullanici_adi: null,
       sifre: null,
-      denemeaq: "",
     };
   },
   methods: {
     tiklama() {
-      console.log(this.kullanici_adi + " - " + this.sifre);
+      let sayac = 0;
+      let bulunduMu = false;
+      this.$axios.get("http://localhost:8000/customers/").then((response) =>
+        response.data.forEach((element) => {
+          sayac++;
+          if (
+            this.kullanici_adi == element["name"] &&
+            this.sifre == element["password"]
+          ) {
+            bulunduMu = true;
+            console.log("Buraya girdi");
+            this.$alert("Giriş Başarılı");
+          } else if (sayac == response.data.length && !bulunduMu) {
+            this.$alert("Giriş Başarısız");
+          }
+        })
+      );
     },
   },
   mounted() {},
@@ -24,11 +64,20 @@ export default {
 };
 </script>
 
+<style>
+@import "https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css";
+</style>
+
 <style scoped>
 #kapsayici {
   text-align: center;
   color: white;
   font-size: 18px;
-  display: list-item;
+}
+
+.input-div {
+  margin: auto;
+  text-align: center;
+  width: 30%;
 }
 </style>
