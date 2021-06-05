1ae562fd5f0a211d9432ab78ae3eb0acae62ea93
RabaxVIII <82673185+RabaxVIII@users.noreply.github.com>
2021-06-05 03:11:32 +0300

    commit

--- src/pages/Maps.vue
+++ src/pages/Maps.vue
@@ -1,8 +1,34 @@
 <template>
   <card type="plain" title="Siparişler">
     <div class="base-demo">
-      <v-data-table :headers="headers" :items="desserts" class="elevation-1">
-      </v-data-table>
+      <template>
+        <v-card>
+          <v-card-title>
+            Sipariş Durumu
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Ara"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+
+          <v-data-table
+            :headers="headers"
+            :items="desserts"
+            :search="search"
+            class="elevation-1"
+          >
+            <template v-slot:[`item.deadline`]="{ item }">
+              <v-chip outlined>
+                {{ item.deadline }}
+              </v-chip>
+            </template>
+          </v-data-table>
+        </v-card>
+      </template>
     </div>
   </card>
 </template>
@@ -11,7 +37,10 @@ import VueTableDynamic from "vue-table-dynamic";
 export default {
   data: function() {
     return {
+      splited: null,
+      splitedDate: null,
       desserts: [],
+      search: "",
       headers: [
         { text: "order_id", value: "order_id" },
         { text: "customer_id", value: "customer_id" },
@@ -28,6 +57,48 @@ export default {
         this.desserts = res.data;
       });
       this.loaded = true;
+    },
+    getColor(date) {
+      this.splited = date.split("-");
+      this.splitedDate = this.currentDateTime;
+      if (this.splited[0] > this.splitedDate[0]) {
+        return "green";
+      } else if (this.splited[0] == this.splitedDate[0]) {
+        if (this.splited[1] > this.splitedDate[1]) {
+          return "green";
+        } else if (this.splited[1] == this.splitedDate[1]) {
+          if (this.splited[2] > this.splitedDate[2]) {
+            return "green";
+          } else if (this.splited[2] == this.splitedDate[2]) {
+            return "orange";
+          } else {
+            return "red";
+          }
+        } else {
+          return "red";
+        }
+      } else {
+        return "red";
+      }
+    },
+    currentDateTime() {
+      const current = new Date();
+      const day0 =
+        current.getDate() < 10 ? "0" + current.getDate() : current.getDate();
+
+      const month0 =
+        current.getMonth() + 1 < 10
+          ? "0" + (current.getMonth() + 1)
+          : current.getMonth() + 1;
+
+      const date = current.getFullYear() + "-" + month0 + "-" + day0;
+
+      const dateTime = date;
+
+      return dateTime;
+    },
+    onButtonClick(item) {
+      console.log("click on " + item.orderi_id);
     }
   },
 
@@ -36,4 +107,4 @@ export default {
   }
 };
 </script>
-<style></style>
+<style scoped></style>
--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -1,14 +1,34 @@
 <template>
   <card type="plain" title="Stoktakiler">
     <div class="base-demo">
-      <v-data-table :headers="headers" :items="desserts" class="elevation-1">
-        <template v-slot:[`item.is_salable`]="{ item }">
-          <v-simple-checkbox
-            v-model="item.is_salable"
-            disabled
-          ></v-simple-checkbox>
-        </template>
-      </v-data-table>
+      <v-card>
+        <v-card-title>
+          Stok
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Ara"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :search="search"
+          class="elevation-1"
+          loading
+          loading-text="Yükleniyor... Lütfen Bekleyiniz."
+        >
+          <template v-slot:[`item.is_salable`]="{ item }">
+            <v-simple-checkbox
+              v-model="item.is_salable"
+              disabled
+            ></v-simple-checkbox>
+          </template>
+        </v-data-table>
+      </v-card>
     </div>
   </card>
 </template>
@@ -19,13 +39,14 @@ export default {
   data: function() {
     return {
       desserts: [],
+      search: "",
       headers: [
         { text: "product_id", value: "product_id" },
         { text: "product_name", value: "product_name" },
         { text: "product_type", value: "product_type" },
-        { text: "is_salable", value: "is_salable" }
-      ],
-      loaded: false
+        { text: "is_salable", value: "is_salable" },
+        { text: "amount", value: "amount" }
+      ]
     };
   },
   components: { VueTableDynamic },
@@ -33,8 +54,16 @@ export default {
     getArray() {
       this.$axios.get("http://127.0.0.1:8000/products/").then(res => {
         this.desserts = res.data;
+        this.$axios.get("http://127.0.0.1:8000/subproducttree/").then(res => {
+          for (let i = 0; i < res.data.length; i++) {
+            for (let j = 0; j < res.data.length; j++) {
+              if (this.desserts[i].product_id == res.data[j].product_id) {
+                this.desserts[i].amount = res.data[j].amount;
+              }
+            }
+          }
+        });
       });
-      this.loaded = true;
     }
   },
 
